f233fd2b4c6c1d8f4cc5e30ebc6cfd3ee254211c
unknown <jimi.lindfors@edu.keuda.fi>
2020-09-09 09:32:01 +0300

    asdfghj

--- lomake.js
+++ lomake.js
@@ -2,7 +2,15 @@ var Etunimi = form.Etunimi.value;
 
 if(Etunimi.length<2)
 {
-  alert("Anna etunimi");
+  alert("Anna etunimi!");
   form.Etunimi.focus();
+  return false;
+}
+var Sukunimi = form.Sukunimi.value;
+
+if(Sukunimi.length<2)
+{
+  alert("Anna sukunimi!");
+  form.Sukunimi.focus();
   return false;
 }
