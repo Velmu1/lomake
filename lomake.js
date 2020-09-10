4e29db0973dfc4087e92d0d34415c412b821c9e3
unknown <olli.manninen@edu.keuda.fi>
2020-09-10 09:28:56 +0300

    aaaaaaaaaaaaaaa

--- lomake.js
+++ lomake.js
@@ -1,52 +1,109 @@
-function lomake()
+function Tyhjennys(form)
 {
-var Etunimi = form.Etunimi.value;
+var tyhj=form.value;
+
+if (!confirm("Haluatko varmasti tyhjentää kaikki kentät?"))
+{
+return false
+}
+
+}
+
+
+/*Lomakkeen kenttien varsinainen tarkistus*/
+
+function Laheta(form)
+{
+
+var Etunimi=form.Etunimi.value;
 
 if(Etunimi.length<2)
 {
-  alert("Anna etunimi!");
-  form.Etunimi.focus();
-  return false;
-}
-}
- {
-  var x = document.getElementByName("salasana").value;
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
-var intPallukka=-1;//radio
-
-for (var i=0; i < form.Pallukka.length;i++)
-  {
-    if (form.Pallukka[i].checked==true)
-  {
-    intPallukka=i;
-  }
-}
-var intLaatikko=-1;//checkbox
-
-for (var i=0; i < form.Laatikko.length;i++)
-  {
-    if (form.Laatikko[i].checked==true)
-  {
-    intLaatikko=i;
-  }
+alert("Et antanut etunimeäsi!");
+form.Etunimi.focus();
+return false;
 }
 
-var Teksti = form.Teksti.value;
+
+var Sukunimi=form.Sukunimi.value;
+
+if(Sukunimi.length<3)
+{
+alert("Et antanut sukunimeäsi!");
+form.Sukunimi.focus();
+return false;
+}
+
+
+if(form.Email.value.indexOf('@', 0) == -1)
+{
+alert("Et antanut sähköpostiosoitettasi tai se on virheellinen!");
+form.Email.focus();
+return false;
+}
+
+
+var Teksti=form.Teksti.value;
 
 if(Teksti.length<10)
 {
-  alert("");
-
-  return false;
+alert("Et kirjoittanut palautteeseesi mitään tekstiä! (min. 10 merkkiä)");
+form.Teksti.focus();
+return false;
 }
+
 if(Teksti.length>1000)
 {
-  alert("");
-
-  return false;
+alert("Palaute-tekstin pituus on rajattu 1000 merkkiin!");
+form.Teksti.focus();
+return false;
 }
+
+
+/*Radio-painikkeiden tarkistus*/
+
+var intVastaus=-1;
+
+for (var i=0; i < form.Vastaus.length;i++) {
+if (form.Vastaus[i].checked==true) {
+intVastaus=i;
+}
+}
+
+
+/*Huomautus, jos radio-painikkeita ei ole valittu*/
+
+if(intVastaus==-1){
+alert("Et valinnut vastataanko palautteeseesi!");
+return false;
+}
+
+
+/*Varmistus-koodin tarkistus*/
+
+var koodi=form.koodi.value;
+
+if(koodi!="292KBX47J6")
+{
+alert("Et kirjoittanut tarkistus-koodia oikein!");
+form.koodi.focus();
+return false;
+}
+
+}
+
+
+/*Tekstikentän pituuden tarkistaminen ja näyttäminen*/
+
+function Tarkistamerkit(form)
+{
+var maxpituus=1000;
+
+var Teksti=form.Teksti.value;
+
+var Merkkejayhteensa=Teksti.length;
+
+form.Merkkejajaljella.value = maxpituus - Merkkejayhteensa;
+
+}
+	
