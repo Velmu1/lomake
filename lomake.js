683260303982e8ea66005ff009c091a8090a0079
unknown <olli.manninen@edu.keuda.fi>
2020-09-11 08:35:36 +0300

    a

--- lomake.js
+++ lomake.js
@@ -1,52 +1,67 @@
-function lomake()
+function tarkasta(form)
 {
-var Etunimi = form.Etunimi.value;
+  var etunimi = form.enimi.value;
+  var salis = form.salasana.value;
+  var palaute = form.Tboksi.value;
+  var pallukka = form.Vastaus.value;
+  var checkbox = form.Mahtava.value;
 
-if(Etunimi.length<2)
-{
-  alert("Anna etunimi!");
-  form.Etunimi.focus();
-  return false;
-}
-}
- {
-  var x = document.getElementByName("salasana").value;
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
+  if(etunimi.length < 3)
+  {
+    alert("Anna vähintään 3-merkkinen etunimi");
+    form.enimi.focus();
+    return false;
+  }
+  if(sukunimi.length < 5)
+   {
+     alert("Anna vähintään 5-merkkinen sukunimi");
+     form.snimi.focus();
+     return false;
+   }
+   if(palaute.length < 20)
+     {
+       alert("Palautteessa pitää olla vähintään 20 merkkiä");
+       form.palaute.focus();
+       return false;
+     }
+     var vastaus = false;
+
+  for(var i = 0; i < pallukka.length; i++)
+  {
+    if(pallukka[i].checked == true)
+    {
+      vastaus = true;
+    }
+  }
+  if(vastaus == false)
+  {
+    alert('Et ole valinnut millainen koneen käyttäjä olet');
+    return false;
+  }
+
+  var checkvastaus = false;
+  for(var j = 0; j < check.length; j++)
+  {
+    if(check[j].checked == true)
+    {
+      checkvastaus = true;
+    }
+  }
+  if(checkvastaus == false)
+  {
+    alert("Et valinnut, mitä mieltä olet sivustani");
+    return false;
+  }
+  else {
+    alert("Kiitos lomakkeen täytöstä");
+
   }
 }
-var intPallukka=-1;//radio
-
-for (var i=0; i < form.Pallukka.length;i++)
-  {
-    if (form.Pallukka[i].checked==true)
-  {
-    intPallukka=i;
-  }
-}
-var intLaatikko=-1;//checkbox
-
-for (var i=0; i < form.Laatikko.length;i++)
-  {
-    if (form.Laatikko[i].checked==true)
-  {
-    intLaatikko=i;
-  }
-}
-
-var Teksti = form.Teksti.value;
-
-if(Teksti.length<10)
+function tyhjenna(lomake)
 {
-  alert("");
-
-  return false;
-}
-if(Teksti.length>1000)
-{
-  alert("");
-
-  return false;
+  var clean = lomake.value;
+  if(!confirm("Oletko varma?"))
+  {
+    return false;
+  }
 }
