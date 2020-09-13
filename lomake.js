50ad14b737d71104df3187deaece5142c19416bd
Your Name <you@example.com>
2020-09-14 00:05:13 +0300

    Valmis

--- lomake.js
+++ lomake.js
@@ -2,26 +2,25 @@ function tarkasta(form)
 {
   var etunimi = form.enimi.value;
   var salis = form.salasana.value;
+  var pallukka = form.pallo;
+  var checkbox = form.mahtava;
   var palaute = form.Tboksi.value;
-  var pallukka = form.Vastaus.value;
-  var checkbox = form.Mahtava.value;
-
   if(etunimi.length < 3)
   {
     alert("Anna vähintään 3-merkkinen etunimi");
     form.enimi.focus();
     return false;
   }
-  if(sukunimi.length < 5)
+  if(salis.length < 5)
    {
      alert("Anna vähintään 5-merkkinen sukunimi");
-     form.snimi.focus();
+     form.salasana.focus();
      return false;
    }
    if(palaute.length < 20)
      {
        alert("Palautteessa pitää olla vähintään 20 merkkiä");
-       form.palaute.focus();
+       form.Tboksi.focus();
        return false;
      }
      var vastaus = false;
@@ -39,15 +38,15 @@ function tarkasta(form)
     return false;
   }
 
-  var checkvastaus = false;
-  for(var j = 0; j < check.length; j++)
+  var mahtava = false;
+  for(var j = 0; j < checkbox.length; j++)
   {
-    if(check[j].checked == true)
+    if(checkbox[j].checked == true)
     {
-      checkvastaus = true;
+      mahtava = true;
     }
   }
-  if(checkvastaus == false)
+  if(mahtava == false)
   {
     alert("Et valinnut, mitä mieltä olet sivustani");
     return false;
@@ -57,6 +56,7 @@ function tarkasta(form)
 
   }
 }
+
 function tyhjenna(lomake)
 {
   var clean = lomake.value;
